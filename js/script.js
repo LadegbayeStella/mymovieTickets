d6f6d750b5f167268ebe85d1b9fe9350af2992a0
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-13 15:54:48 +0100

    adding my last commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 // business logic
-function Tickets() {
-  this.customers
+function Cinema() {
+  this.movies={};
+  this.currentId = 0;
 };
+// constructor
+function Movie(title, time, age,price,date) {
+  this.title = title;
+  this.time = time;
+  this.age= age;
+  this.price=price;
+  this.date=date;
+}
+function customer(firstName,lastName,age,eMail, movieid){
+this.firstName =firstName;
+this.lastName =lastName;
+this.age =age;
+this.eMail = eMail;
+this.Ticketsids [];
+}
+
 // method to assign id
 Tickets.prototype.assignId = function () {
   this.currentId += 1;
@@ -11,6 +28,11 @@ Tickets.prototype.assignId = function () {
 // method to add customers
 Tickets.prototype.addCustomers =function(customers){
   customers.id = this.assignId();
- this.customers[customer.id] = customer;
+ this.customers[customers.id] = customers;
 }
+addmovie
 
+// method for getting price
+Tickets.prototype.getPrice =function(customer){
+  let price = this.movies[customer.movie]
+}
