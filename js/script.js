5f85f5c4a2c39019122ff675a1a0ee80383a5526
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-14 12:46:50 +0100

    --- Auto Git Commit ---

--- js/script.js
+++ js/script.js
@@ -1,41 +1,6 @@
-// business logic
-function Cinema() {
-  this.movies = {};
-  this.currentId = 0;
-};
-// constructor
-function Movie(title, time, age, price, date) {
-  this.title = title;
-  this.time = time;
-  this.age = age;
-  this.price = price;
-  this.date = date;
-}
-function customer(firstName, lastName, age, eMail, movieid) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.eMail = eMail;
-  this.Ticketsids;
-}
 
-// method to assign id
-Tickets.prototype.assignId = function () {
-  this.currentId += 1;
-  return this.currentId;
-};
-
-// method to add customers
-Tickets.prototype.addCustomers = function (customers) {
-  customers.id = this.assignId();
-  this.customers[customers.id] = customers;
-}
-addmovie
-
-// method for getting price
-Tickets.prototype.getPrice = function (customer) {
-  let price = this.movies[customer.movie]
-}
+ 
+// Bussiness logic
 let myMovieDob =newMovie;
 
 
@@ -69,5 +34,5 @@ Movies.prototype.addCustomers = function (customers) {
   customers.id = this.assignId();
   this.customers[customers.id] = customers;
 }
-let stella = new Customer ("stella","ladegbaye","14-07-2022")
+let stella = new Customer ("stella","ladegbaye","doctorStrange", 12 ,"eMail");
 myMovieDob.addStudent(stella)
