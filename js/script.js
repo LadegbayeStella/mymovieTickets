53cbbebe1f9c8dccf3b2972104a496d00533f28e
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-14 13:47:22 +0100

    --- Auto Git Commit ---

--- js/script.js
+++ js/script.js
@@ -4,35 +4,28 @@
 let myMovieDob =newMovie;
 
 
-function Movies() {
-  this.Movie = {};
-  this.currentId = 0
+function Cineama() {
+  this.Movies = {};
+  this.currentId = 0;
 }
 // contructor for movie
-function Movie(title, time, age, price, date) {
+function Movies( firstName, lastName,title, time, age, price, date) {
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.title = title;
   this.time = time;
   this.age = age;
   this.price = price;
   this.date = date;
 }
-// constructor for customer
-function Customers(firstName, lastName, age, eMail, movieid) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.eMail = eMail;
-  this.Ticketsids;
-}
+
 // add method to assignId
 Movies.prototype.assignId =function(){
   this .currentId =this.currentId+1
   return this.currentId;
 }
 // method to add customer
-Movies.prototype.addCustomers = function (customers) {
-  customers.id = this.assignId();
-  this.customers[customers.id] = customers;
+Movies.prototype.addMovies = function () {
+Movies.id = this.assignId();
+this.Movies[customers.id] = customers;
 }
-let stella = new Customer ("stella","ladegbaye","doctorStrange", 12 ,"eMail");
-myMovieDob.addStudent(stella)
