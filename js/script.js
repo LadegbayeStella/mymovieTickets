ef119cbd85bdc24a1ebd210ef5bc28087dd74947
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-14 12:16:33 +0100

    --- Auto Git Commit ---

--- js/script.js
+++ js/script.js
@@ -36,26 +36,38 @@ addmovie
 Tickets.prototype.getPrice = function (customer) {
   let price = this.movies[customer.movie]
 }
+let myMovieDob =newMovie;
 
-function Movie() {
-  this.customer = {};
+
+function Movies() {
+  this.Movie = {};
   this.currentId = 0
 }
-function Customer(firstName, lastName, movieTitle) {
+// contructor for movie
+function Movie(title, time, age, price, date) {
+  this.title = title;
+  this.time = time;
+  this.age = age;
+  this.price = price;
+  this.date = date;
+}
+// constructor for customer
+function Customers(firstName, lastName, age, eMail, movieid) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.doa = Doa
+  this.age = age;
+  this.eMail = eMail;
+  this.Ticketsids;
 }
-movie.prototype.assignId =function(){
+// add method to assignId
+Movies.prototype.assignId =function(){
   this .currentId =this.currentId+1
   return this.currentId;
 }
-
-movie.prototype.addStudent =function(student){
-  let studentId = this.assignid();
-  this.students[studentId]=student;
-  student.id=studentId;
+// method to add customer
+Movies.prototype.addCustomers = function (customers) {
+  customers.id = this.assignId();
+  this.customers[customers.id] = customers;
 }
-let stella = new Student("stella","ladegbaye","14-07-2022")
-let myRegisterDob =newRegister;
-MediaEncryptedEvent.addStudent(stella)
+let stella = new Customer ("stella","ladegbaye","14-07-2022")
+myMovieDob.addStudent(stella)
