062f00079bb68514cbe5c3a36ca7955299fe8c93
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-14 15:28:33 +0100

    --- Auto Git Commit ---

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
- 
+
 // Bussiness logic
-let myMovieDob =newMovie;
+let myMovieDob = newMovies;
 
 
 function Cineama() {
@@ -9,7 +9,7 @@ function Cineama() {
   this.currentId = 0;
 }
 // contructor for movie
-function Movies( firstName, lastName,title, time, age, price, date) {
+function Movies(firstName, lastName, title, time, age, price, date) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.title = title;
@@ -20,12 +20,31 @@ function Movies( firstName, lastName,title, time, age, price, date) {
 }
 
 // add method to assignId
-Movies.prototype.assignId =function(){
-  this .currentId =this.currentId+1
+Movies.prototype.assignId = function () {
+  this.currentId = this.currentId + 1
   return this.currentId;
 }
 // method to add customer
 Movies.prototype.addMovies = function () {
-Movies.id = this.assignId();
-this.Movies[Movies.id] =Movies;
+  Movies.id = this.assignId();
+  this.Movies[Movies.id] = Movies;
 }
+Movies.prototype.fullTicket = function () {
+  return this.firstName +","+ this.lastName +"," +this.movie + ", " + this.time + ", " + this.age;
+}
+
+$(document).ready(function () {
+  $("#movie-ticket").submit(function (event) {
+    event.preventDefault();
+    let movie = $("movie-title").val();
+    let time = $("#time").val();
+    let age = $("#age").val();
+    $("#output").show();
+    // let ticketInfo = new Movies(firstName, lastName, movie, time, age);
+    // const ticketDisplay = ticketInfo.fullTicket();
+    // const ticketCost = ticketInfo.calculateCost();
+    // $("#output").text(ticketDisplay);
+    // $("#cost").text(ticketCost);
+  })
+
+});
