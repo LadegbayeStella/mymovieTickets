efccaa331a8ca0a4c7bde18cdb30243279c07da9
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-07-14 15:58:48 +0100

    --- Auto Git Commit ---

--- js/script.js
+++ js/script.js
@@ -1,50 +1,75 @@
 
 
-// Bussiness logic
-let myMovieDob = newMovies;
+// // Bussiness logic
+// // let myMovieDob = newMovies;
 
 
-function Cineama() {
-  this.Movies = {};
-  this.currentId = 0;
-}
-// contructor for movie
-function Movies(firstName, lastName, title, time, age, price, date) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+// function Cineama() {
+//   this.Movies = {};
+//   this.currentId = 0;
+// }
+// // contructor for movie
+// function Movies(firstName, lastName, title, time, age, price, date) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.title = title;
+//   this.time = time;
+//   this.age = age;
+//   this.price = price;
+//   this.date = date;
+// }
+
+// // add method to assignId
+// Movies.prototype.assignId = function () {
+//   this.currentId = this.currentId + 1
+//   return this.currentId;
+// }
+// // method to add customer
+// Movies.prototype.addMovies = function () {
+//   Movies.id = this.assignId();
+//   this.Movies[Movies.id] = Movies;
+// }
+// Movies.prototype.fullTicket = function () {
+//   return this.firstName + "," + this.lastName + "," + this.movie + ", " + this.time + ", " + this.age;
+// }
+
+// $(document).ready(function () {
+//   $("#movie-ticket").submit(function (event) {
+//     event.preventDefault();
+//     let movie = $("movie-title").val();
+//     let time = $("#time").val();
+//     let age = $("#age").val();
+//     $("#output").show();
+//     // let ticketInfo = new Movies(firstName, lastName, movie, time, age);
+//     // const ticketDisplay = ticketInfo.fullTicket();
+//     // const ticketCost = ticketInfo.calculateCost();
+//     // $("#output").text(ticketDisplay);
+//     // $("#cost").text(ticketCost);
+//   })
+
+// });
+
+function Ticket(title, showtime, age) {
   this.title = title;
-  this.time = time;
+  this.showtime = showtime;
   this.age = age;
-  this.price = price;
-  this.date = date;
+}
+Ticket.prototype.ticketPrice = function() {
+  return this.title + this.showtime + this.age;
 }
 
-// add method to assignId
-Movies.prototype.assignId = function () {
-  this.currentId = this.currentId + 1
-  return this.currentId;
-}
-// method to add customer
-Movies.prototype.addMovies = function () {
-  Movies.id = this.assignId();
-  this.Movies[Movies.id] = Movies;
-}
-Movies.prototype.fullTicket = function () {
-  return this.firstName +","+ this.lastName +"," +this.movie + ", " + this.time + ", " + this.age;
-}
-
-$(document).ready(function () {
-  $("#movie-ticket").submit(function (event) {
+$(document).ready(function() {
+  $("#movie-ticket").submit(function(event) {
     event.preventDefault();
-    let movie = $("movie-title").val();
-    let time = $("#time").val();
-    let age = $("#age").val();
-    $("#output").show();
-    // let ticketInfo = new Movies(firstName, lastName, movie, time, age);
-    // const ticketDisplay = ticketInfo.fullTicket();
-    // const ticketCost = ticketInfo.calculateCost();
-    // $("#output").text(ticketDisplay);
-    // $("#cost").text(ticketCost);
-  })
-
+    const inputtedMovie = $('#movie-selection').val();
+    const inputtedShowtime = $('#showtime-selection').val();
+    const inputtedAge = $('#age-selection').val();
+    const intMovie = parseInt(inputtedMovie);
+    const intShowtime = parseInt(inputtedShowtime);
+    const intAge = parseInt(inputtedAge);
+    let ticketRequest = new Ticket(intMovie, intShowtime, intAge);
+    let price = ticketRequest.ticketPrice();
+    $("#ticket-total").show();
+    $("#cost").html(price);
+  });
 });
